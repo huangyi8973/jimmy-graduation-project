14c6bb935fc379cde7df714f1943fcf2d27a7c12
huangyi8973@gmail.com <huangyi8973@gmail.com@2dd73c2d-61dd-ade4-081c-b2eb2076bc96>
2012-05-22 09:21:31 +0000

    MD5加密类

---
+++ trunk/Mis/Mis/Mis.Core/Utilty/MD5Tool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Mis.Core.Utilty
+{
+    public class MD5Tool
+    {
+        public static string Parse(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] result = md5.ComputeHash(System.Text.Encoding.Default.GetBytes(str));
+            string str2 = "";
+            for (int i = 0; i < result.Length; i++)
+            {
+                str2 += string.Format("{0:x}", result[i]);
+            }
+            return str2;
+        }
+    }
+}
